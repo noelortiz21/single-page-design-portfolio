97166ecd20782ac389d8b1eb755328ee5b366f01
Noel Ortiz <noel@Noels-MacBook-Pro-2.local>
2024-05-30 12:15:41 -0700

    updated starter template

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,20 @@
-import { resolve } from "path";
-import { defineConfig } from "vite";
+import htmlPurge from "vite-plugin-html-purge";
 
-export default defineConfig({
-  build: {
-    rollupOptions: {
-      input: {
-        home: resolve(__dirname, "index.html"),
-        about: resolve(__dirname, "about/index.html"),
-        contact: resolve(__dirname, "contact/index.html"),
-      },
-    },
-  },
-});
+export default {
+  plugins: [htmlPurge()],
+};
+
+// import { resolve } from "path";
+// import { defineConfig } from "vite";
+
+// export default defineConfig({
+//   build: {
+//     rollupOptions: {
+//       input: {
+//         home: resolve(__dirname, "index.html"),
+//         about: resolve(__dirname, "about/index.html"),
+//         contact: resolve(__dirname, "contact/index.html"),
+//       },
+//     },
+//   },
+// });
